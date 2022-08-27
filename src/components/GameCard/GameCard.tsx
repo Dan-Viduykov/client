738818fac17f5fa28f3edc289520c9a829bbc981
Danya_Vid <danilviduykov@gmail.com>
2022-08-27 13:02:42 +0500

    some fix

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -1,4 +1,4 @@
-import React, { FC, MouseEvent, useEffect, useState } from "react";
+import React, { FC, MouseEvent, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { IGame } from "../../services/game.types";
